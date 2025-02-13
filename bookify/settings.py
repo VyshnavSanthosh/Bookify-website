c1c6a145fd7e22c84bfb5d0807a45b4aee2bcb79
VyshnavSanthosh <vyshnavsanthosh5@gmail.com>
2025-02-13 20:08:33 +0530

    add requirements

--- bookify/settings.py
+++ bookify/settings.py
@@ -132,10 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Add this line
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
